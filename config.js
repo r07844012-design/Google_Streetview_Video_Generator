4ac539053c4346d72d61a66c5ed888346c739d4d
r07844012-design <r07844012@ntu.edu.tw>
2026-07-10 16:36:05 +0800

    Second Version

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // 全域設定管理
 const CONFIG = {
-    GOOGLE_CLIENT_ID: "313542094086-1ggi1ebis3dili6emv96a7hhnnkpu2l0.apps.googleusercontent.com",
-    GOOGLE_MAPS_API_KEY: "e53160fb1e8e1f7b838d522fffa80ed6ddab0f17765b51d9e21e7758ab9a05bb"
+    GOOGLE_CLIENT_ID: "972823345672-l3u8c01j9tbteb7b4eq2g46lsn5va0dt.apps.googleusercontent.com",
+    GOOGLE_MAPS_API_KEY: "AIzaSyCDdsH6ibjD6Hq48ssFPx86GsO5UUsSZdw"
 };
